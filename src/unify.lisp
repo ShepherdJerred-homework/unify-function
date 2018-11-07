82b90a4029e778fada1cd1813b362a481688f9dc
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-11-06 22:52:05 -0600

    Work on impl

--- src/unify.lisp
+++ src/unify.lisp
@@ -1,3 +1,58 @@
+(defun is-nil-list
+  (term)
+  (if
+    (eq term '(nil))
+    T
+    nil))
+
+(defun both-null
+  (t1 t2)
+  (if
+    (and
+      (null t1)
+      (null t2))
+    T
+    nil))
+
+(defun either-null
+  (t1 t2)
+  (if
+    (or
+      (null t1)
+      (null t2))
+    T
+    nil))
+
+(defun both-nil-list
+  (t1 t2)
+  (if
+    (and
+      (is-nil-list t1)
+      (is-nil-list t2))
+    T
+    nil))
+
+(defun merge-two-lists
+  (t1 t2)
+  (if
+    (is-nil-list t1)
+    t2
+    (if
+      (is-nil-list t2)
+      t1
+      (list t1 t2))))
+
+(defun merge-three-lists
+  (m1 m2 sigma)
+  (merge-two-lists m1 (merge-two-lists m2 sigma)))
+
+(defun merge-sigma
+  (m1 m2 sigma)
+  (if
+    (is-nil-list sigma)
+    (merge-two-lists m1 m2)
+    (merge-three-lists m1 m2 m3)))
+    
 (defun do-unify
   (t1 t2 sigma)
   (let
@@ -5,17 +60,15 @@
       (m1 (match t1 t2))
       (m2 (match t2 t1)))
     (if
-      (or
-        (null m1)
-        (null m2))
-      (cons sigma (cons m1 (cons m2 nil)))
+      (both-null m1 m2)
+      nil
       (if
-        (and
-          (eq m1 '(nil))
-          (eq m2 '(nil)))
+        (both-nil-list m1 m2)
         sigma
-        (do-unify (simplify t1 m1) (simplify t2 m2) (cons sigma (cons m1 (cons m2 nil))))))))
-      
+        (let
+          (
+            (new-sigma (merge-sigma m1 m2 sigma)))
+          (do-unify (simplify t1 new-sigma) (simplify t2 new-sigma) new-sigma))))))
 
 (defun unify
   (t1 t2)
--- test/test.lisp
+++ test/test.lisp
@@ -3,27 +3,35 @@
   (load "../src/simplify.lisp")
   (load "../src/unify.lisp")
   (trace unify)
-  (trace do-unify))
+  (trace do-unify)
+  (trace match)
+  (trace simplify)
+  (trace is-nil-list)
+  (trace either-null)
+  (trace both-nil-list)
+  (trace merge-two-lists)
+  (trace merge-three-lists)
+  (trace merge-sigma))
 
 (and
   (equal
     (unify '(+ a b) '(+ a x))
-    ((X . B)))
+    '((X . B)))
   (equal
     (unify '(+ a b) '(x a b))
-    ((X . +)))
+    '((X . +)))
   (equal
     (unify '(+ (- b c) a) '(+ x y))
-    ((Y . A) (X - B C)))
+    '((Y . A) (X - B C)))
   (equal
     (unify '(+ Y B) '(+ A X))
-    ((X . B) (Y . A)))
+    '((X . B) (Y . A)))
   (equal
     (unify '(* A Z) '(* Y Y))
-    ((Z . A) (Y . A)))
+    '((Z . A) (Y . A)))
   (equal
     (unify '(+ (- B C) Y) '(+ X A))
-    ((Y . A) (X - B C)))
+    '((Y . A) (X - B C)))
   (equal
     (unify '(+ Z (+ B A)) '(+ C (+ Y X)))
-    ((X . A) (Y . B) (Z . C))))
+    '((X . A) (Y . B) (Z . C))))
